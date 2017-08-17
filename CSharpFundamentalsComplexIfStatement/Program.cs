f5ec3ac1ca44c5c5f6fa930e4efd56a68bd54e80
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-17 07:25:32 -0500

    added example if else if else stetement

--- CSharpFundamentalsComplexIfStatement/Program.cs
+++ CSharpFundamentalsComplexIfStatement/Program.cs
@@ -10,7 +10,19 @@ namespace CSharpFundamentalsComplexIfStatement
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pick a number between 1 and 100:");
+            string userValue = Console.ReadLine();
 
+            int compareValue = int.Parse(userValue);
+
+            if ((compareValue < 1)  || (compareValue > 100))
+                Console.WriteLine("The number you chose was out of bounds.");
+            else if ((compareValue == 42) || (compareValue > 90))
+                Console.WriteLine("You found one of the special numbers!!!");
+            else
+                Console.WriteLine("You didn't find one of the special numbers.");
+
+            Console.ReadLine();
         }
     }
 }
